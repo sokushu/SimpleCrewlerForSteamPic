b6415132f0badbbe9036cc1cf50c7cd1f29eca78
looko <31150545+lookoutldz@users.noreply.github.com>
2018-02-08 18:05:49 +0800

    encapsulation_1

--- src/launch/Launcher.java
+++ src/launch/Launcher.java
@@ -7,12 +7,14 @@ public class Launcher {
 
     public static void main(String[] args){
 
+        String urlstr = "http://store.steampowered.com/agecheck/app/637650";
+        //637650
         //524220
         //374320
         //668630
         //570
 
-        new Connector().sentRequest(524220);
+        new Connector().sentRequest(urlstr);
         //HTMLs.readFile();
         //HTMLs.catch_1(HTMLs.readFile());
         HTMLs.catch_2(HTMLs.catch_1(HTMLs.readFile()));
