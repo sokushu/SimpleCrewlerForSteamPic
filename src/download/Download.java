55eecbb6d93f323af418891729c5951a75b23d54
Mirukk <mapyajapan@yahoo.co.jp>
2018-02-11 22:56:07 +0800

    更新

--- src/download/Download.java
+++ src/download/Download.java
@@ -1,4 +1,68 @@
 package download;
 
+import link.ToGet;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Download {
+    private String[] imgUrl;
+    private String FilePath;
+    public Download(String[] imgUrl, String FilePath){
+        this.imgUrl = imgUrl;
+        this.FilePath = FilePath;
+        File file = new File(FilePath);
+        if (!file.exists()){
+            file.mkdir();
+        }
+        for(int i = 0; i < imgUrl.length; i++){
+            saveImg(imgUrl[i].toString());
+        }
+    }
+
+    private InputStream inputStream;
+
+    private void saveImg(String imgUrl){
+        try {
+            URL url = new URL(imgUrl);
+            HttpURLConnection httpcon = ToGet.sentGET(imgUrl,null);
+
+            inputStream = httpcon.getInputStream();
+
+            int a = 0;
+            byte[] bData = new byte[1024];
+            ByteArrayOutputStream bytearr = new ByteArrayOutputStream();
+            while ((a = inputStream.read(bData)) != -1){
+                bytearr.write(bData, 0, a);
+            }
+            bytearr.close();
+
+            byte[] data = bytearr.toByteArray();
+            String filename = filename(imgUrl);
+            File savefile = new File(FilePath + File.separator + filename);
+            FileOutputStream fileout = new FileOutputStream(savefile);
+            fileout.write(data);
+            fileout.close();
+            inputStream.close();
+            System.out.print("OK( ^v^ )b " + FilePath + filename);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static String filename(String imgUrl){
+        String[] pic = {".jpg",".png",".jpeg",".JPG",".JPEG",".PNG",".gif",".GIF",".bmp",".BMP"};
+        String filename = "";
+        for (int i = 0; i < pic.length; i++) {
+            if (imgUrl.indexOf(pic[i].toString()) > -1) {
+                filename = imgUrl.substring(imgUrl.lastIndexOf("/") + 1, imgUrl.indexOf(pic[i])) + pic[i];
+            }
+            break;
+        }
+        return filename;
+    }
 }
--- src/launch/Launcher.java
+++ src/launch/Launcher.java
@@ -1,19 +1,21 @@
 package launch;
 
+import download.Download;
 import link.ConManager;
 
 public class Launcher {
 
     public static void main(String[] args){
 
-        String urlstr = "http://store.steampowered.com/app/220";
-        //637650
-        //524220
-        //374320
-        //668630
-        //570
-        //292030
-        new ConManager().getPic(urlstr);
+//        String urlstr = "http://store.steampowered.com/app/220";
+//        //637650
+//        //524220
+//        //374320
+//        //668630
+//        //570
+//        //292030
+//        new ConManager().getPic(urlstr);
+        new Download(new String[]{"http://cdn.steamstatic.com.8686c.com/steam/apps/220/0000001869.1920x1080.jpg?t=1515390717"},"E:\\Work\\MyProject\\SimpleCrewlerForSteamPic");
 
     }
 }
